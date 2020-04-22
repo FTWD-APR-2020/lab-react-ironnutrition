eefc892f8d7283e0086b224e1b40be2e1c38f8a4
Juan Grajales <grajales.juanfe11@gmail.com>
2020-04-22 14:49:57 -0400

    iteration1-2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
+  state = {
+    allFoods: foods
+  }
+
+  displayFoods = () => {
+    return this.state.allFoods.map(eachFood => {
+      return (<FoodBox key={eachFood.name} name={eachFood.name} calories={eachFood.calories} image={eachFood.image}/>)
+    })
+  }
+  
   render() {
+    console.log(foods)
     return (
       <div className="App">
         Wazzaaa
+        {this.displayFoods()}
       </div>
     );
   }
